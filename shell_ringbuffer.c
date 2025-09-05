d0316a90604c70b89616c88a500b5d881b6c3ff9
Diveturtle93 <Hafemeister1@hotmail.de>
2025-09-05 06:28:42 +0200

    HInzufügen Shell Ringbuffer
    - Anlegen Typedefine
    - Initialisiere Ringbuffer
    - Hinzufügen zum Ringbuffer
    - Entfernen von Ringbuffer
    - Prüfen ob Ringbuffer voll
    - Prüfen on Ringbuffer leer

---
+++ shell_ringbuffer.c
@@ -0,0 +1,109 @@
+//----------------------------------------------------------------------
+// Titel	:	shell_queue.c
+//----------------------------------------------------------------------
+// Sprache	:	C
+// Datum	:	05.09.2025
+// Version	:	1.0
+// Autor	:	Diveturtle93
+// Projekt	:	Shell
+// Quelle	:	https://github.com/ShareCat/STM32CommandLine
+//				https://github.com/mdiepart/ushell-stm32
+//----------------------------------------------------------------------
+
+// Einfuegen der standard Include-Dateien
+//----------------------------------------------------------------------
+#include <string.h>
+//----------------------------------------------------------------------
+
+// Einfuegen der STM Include-Dateien
+//----------------------------------------------------------------------
+#include "main.h"
+//----------------------------------------------------------------------
+
+// Einfuegen der eigenen Include Dateien
+//----------------------------------------------------------------------
+#include "shell_queue.h"
+//----------------------------------------------------------------------
+
+// Initialisiere Ringbuffer
+//----------------------------------------------------------------------
+bool shell_ringbuffer_init (RingbufferShellTypeDef *ring)
+{
+	ring->head = 0;
+	ring->tail = 0;
+
+    memset(ring->PBase, 0, SHELL_RING_LENGTH);
+
+    return true;
+}
+//----------------------------------------------------------------------
+
+// Zeichen zum Ring hinzufuegen
+//----------------------------------------------------------------------
+bool shell_addToRingBuffer (RingbufferShellTypeDef *ring, uint8_t *PData)
+{
+    // Variable definieren
+	uint16_t nextEntry;
+	nextEntry = (ring->head + 1) % ring->size;
+	
+	// Fuege Element zum Ring hinzu
+    ring->PBase[ring->tail] = *PData;
+	
+	// Ringbuffer Kopf hochzaehlen
+    ring->tail = ((ring->tail) + 1) % SHELL_RING_LENGTH;
+
+    return true;
+}
+//----------------------------------------------------------------------
+
+// Zeichen von Ring entfernen
+//----------------------------------------------------------------------
+bool shell_removeFromRingBuffer(RingbufferShellTypeDef *ring, uint8_t *PData)
+{
+	// Pruefen, ob Zeichen im Ring sind
+    if (shell_isRingBufferEmpty(ring))
+	{
+        return false;
+    }
+	
+	// Kopiere Zeichen
+    *PData = ring->PBase[ring->tail];
+	
+	// Ringbuffer Schwanz hochzaehlen
+    ring->tail = (ring->tail + 1) % SHELL_RING_LENGTH;
+
+    return true;
+}
+//----------------------------------------------------------------------
+
+// Abfrage, ob Ringbuffer voll ist
+//----------------------------------------------------------------------
+bool shell_ringbuffer_full (RingbufferShellTypeDef *ring)
+{
+	
+    if ((((ring->tail) + 1) % SHELL_RING_LENGTH) == ring->head)
+	{
+        return true;														// Wenn voll, Rueckgabe True
+    }
+	else
+	{
+        return false;														// Wenn voll, Rueckgabe False
+    }
+}
+//----------------------------------------------------------------------
+
+// Abfrage, ob Ringbuffer leer ist
+//----------------------------------------------------------------------
+bool shell_isRingBufferEmpty (RingbufferShellTypeDef *ring)
+{
+	// Wenn Ringpuffer leer
+	if (ring->head == ring->tail)
+	{
+		return true;														// Wenn leer, Rueckgabe True
+	}
+	else
+	{
+		return false;														// Wenn leer, Rueckgabe False
+	}
+}
+//----------------------------------------------------------------------
---
+++ shell_ringbuffer.h
@@ -0,0 +1,46 @@
+//----------------------------------------------------------------------
+// Titel	:	shell_ringbuffer.h
+//----------------------------------------------------------------------
+// Sprache	:	C
+// Datum	:	05.09.2025
+// Version	:	1.0
+// Autor	:	Diveturtle93
+// Projekt	:	Shell
+// Quelle	:	https://github.com/ShareCat/STM32CommandLine
+//				https://github.com/mdiepart/ushell-stm32
+//----------------------------------------------------------------------
+
+// Dateiheader definieren
+//----------------------------------------------------------------------
+#ifndef SHELL_RINGBUFFER_H_
+#define SHELL_RINGBUFFER_H_
+//----------------------------------------------------------------------
+
+// Konstanten definieren
+//----------------------------------------------------------------------
+#ifndef SHELL_RING_LENGTH
+	#define SHELL_RING_LENGTH 32											// Laenge ist default auf 32 Zeichen gesetzt
+#endif
+//----------------------------------------------------------------------
+
+// Typedefine definieren
+//----------------------------------------------------------------------
+typedef struct ringbuffer
+{
+	size_t		head;														//
+	size_t 		tail;														//
+	uint8_t		PBase[SHELL_QUEUE_LENGTH];									//
+} RingbufferShellTypeDef;
+//----------------------------------------------------------------------
+
+// Funktionen definieren
+//----------------------------------------------------------------------
+bool shell_ringbuffer_init (RingbufferShellTypeDef *ring);					//
+bool shell_addToRingBuffer (RingbufferShellTypeDef *ring, uint8_t *PData);	//
+bool shell_removeFromRingBuffer (RingbufferShellTypeDef *ring, uint8_t *PData);	//
+bool shell_ringbuffer_full (RingbufferShellTypeDef *ring);					//
+bool shell_isRingBufferEmpty (RingbufferShellTypeDef *ring);				//
+//----------------------------------------------------------------------
+
+#endif /* SHELL_RINGBUFFER_H_ */
+//----------------------------------------------------------------------
